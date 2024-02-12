016366f150f292eb970d771a6b6aca1d6f7cabc2
mrobeidat <y.linux96@gmail.com>
2024-02-12 22:35:12 +0300

    replace play/pause icons

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -19,14 +19,14 @@ const Player: React.FC<PlayerProps> = () => {
   };
 
   return (
-    <div className="bg-black flex h-50 w-377 rounded-lg p-2 items-center gap-2">
+    <div className="bg-black flex h-50 w-377 rounded-lg p-3 items-center gap-2">
       <audio src="https://samples-files.com/samples/Audio/mp3/sample-file-2.mp3"></audio>
       <Image src={Images.Backward} alt="backward" />
       <button onClick={togglePlayPause}>
         {isPlaying ? (
-          <Image src={Images.Pause} alt="play" className="mt-2" />
+          <Image src={Images.Pause} alt="pause" />
         ) : (
-          <Image src={Images.Play} alt="play" className="mt-2" />
+          <Image src={Images.Play} alt="play" />
         )}
       </button>
       <Image src={Images.Forward} alt="forward" />
