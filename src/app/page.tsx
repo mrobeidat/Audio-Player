1c8f273a00e5b4b262aedd7b204d63acc4de6ad1
mrobeidat <y.linux96@gmail.com>
2024-02-13 19:02:53 +0300

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,135 +1,19 @@
-
 "use client";
 import React, { useEffect } from "react";
 import { Player } from "./components/Player";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Particles from "react-particles";
-
 const Home: React.FC = () => {
   useEffect(() => {
     AOS.init({
-      duration: 700, 
+      duration: 400, 
       easing: "ease-in",
     });
     AOS.refresh();
   }, []);
-
   return (
-    <div className="relative">
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 0,
-                color: "#000000",
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 40,
-                size_min: 0.1,
-                sync: false,
-              },
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-              anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: true,
-              distance: 150,
-              color: "#ffffff",
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 6,
-              direction: "none",
-              random: false,
-              straight: false,
-              out_mode: "out",
-              bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
-          },
-          interactivity: {
-            detect_on: "canvas",
-            events: {
-              onhover: {
-                enable: true,
-                mode: "repulse",
-              },
-              onclick: {
-                enable: true,
-                mode: "push",
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
-          },
-          retina_detect: true,
-        }}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          zIndex: 0,
-        }}
-      />
-      <div className="flex h-screen items-center justify-center p-24 bg-gradient-to-r from-customPurple to-red-500  via-pink-800">
-        <Player />
-      </div>
+    <div className="flex h-screen items-center justify-center p-24 bg-gradient-to-r from-customPurple to-red-500  via-pink-800">
+      <Player />
     </div>
   );
 };
