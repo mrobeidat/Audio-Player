b1a1d9c89327e2511007bef8a3576fa30773b0b2
mrobeidat <y.linux96@gmail.com>
2024-02-19 02:22:30 +0300

    test db

--- libs/mongodb.tsx
+++ libs/mongodb.tsx
@@ -3,13 +3,10 @@ import mongoose from "mongoose";
 // connect to MongoDB database using Mongoose.
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI || process.env.MONGODB_URI_TEST,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      } as mongoose.ConnectOptions
-    );
+    await mongoose.connect(process.env.MONGODB_URI_TEST, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
