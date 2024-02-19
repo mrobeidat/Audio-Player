503e9575b6621bd3335d5e385109c9b0291c9540
mrobeidat <y.linux96@gmail.com>
2024-02-20 02:56:15 +0300

    rever the changes

--- src/app/pages/player/page.tsx
+++ src/app/pages/player/page.tsx
@@ -2,6 +2,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import { Images } from "../../components/Shared/Media/Images"; // Importing Images for audio controls
+import Audios from "../../components/Shared/Media/Audio"; // Importing the audio file
 import ParticleStart from "../../components/Shared/Particles/ParticleStart";
 import Footer from "../../layout/Footer";
 import AOS from "aos"; // animate on scroll
@@ -9,40 +10,48 @@ import "aos/dist/aos.css"; // animate on scroll styles
 import ParticleEnd from "../../components/Shared/Particles/ParticleEnd";
 import { PuffLoader } from "react-spinners";
 import { AudioList } from "../../components/Shared/Media/AudioList";
-import { playlist } from "../../components/Shared/Media/AudioList";
+
 interface PlayerProps {}
 
 const Player: React.FC<PlayerProps> = () => {
-  // state of the audio player controls
-  const [playerState, setPlayerState] = useState({
-    isPlaying: false,
-    isMuted: false,
-    duration: 0,
-    currentTime: 0,
-    currentSongIndex: 1,
-    textMove: 0,
-  });
+  const [loading, setLoading] = useState(true);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [duration, setDuration] = useState<number>(0);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
+  const [TextMove, setTextMove] = useState(0);
 
-  // reference to loading state, audio player, progress bar, and animation
-  const [audioState, setAudioState] = useState({
-    loading: true,
-    audioPlayer: useRef(null),
-    progressBar: useRef(null),
-    animationRef: useRef(null),
-  });
+  const audioPlayer = useRef(null); // reference to audio player
+  const progressBar = useRef(null); // reference to progress bar
+  const animationRef = useRef(null); // reference to animation
 
-  // Loading the meta data on mount
-  const audioElement = audioState.audioPlayer.current;
+  const [playlist, setPlaylist] = useState<string[]>([
+    Audios.Song5,
+    Audios.Song1,
+    Audios.Song2,
+    Audios.Song3,
+    Audios.Song4,
+  ]);
+
+  // Initializing AOS animations to set up animations on component mount
+  useEffect(() => {
+    AOS.init({
+      duration: 400,
+      easing: "ease-in",
+    });
+    AOS.refresh();
+  }, []);
+
+  const audioElement = audioPlayer.current;
   const handleLoadedMetadata = () => {
     const seconds = Math.floor(audioElement.duration);
-    setPlayerState((prevState) => ({
-      ...prevState,
-      duration: seconds,
-    }));
-    audioState.progressBar.current.max = String(seconds);
+    setDuration(seconds);
+    progressBar.current.max = String(seconds);
   };
+
   useEffect(() => {
-    if (!audioElement) return;
+    if (!audioElement) return; // Check if audioElement is null
     audioElement.addEventListener("loadedmetadata", handleLoadedMetadata);
     return () => {
       audioElement.removeEventListener("loadedmetadata", handleLoadedMetadata);
@@ -71,18 +80,16 @@ const Player: React.FC<PlayerProps> = () => {
 
   // To play and pause the audio player
   const togglePlayPause = () => {
-    const { audioPlayer, animationRef } = audioState;
-    const { isPlaying, currentSongIndex } = playerState;
-
     if (!audioPlayer.current) return;
 
-    if (!isPlaying) {
-      setPlayerState((prevState) => ({ ...prevState, isPlaying: true }));
+    const previousState = isPlaying;
+    setIsPlaying(!previousState);
+
+    if (!previousState) {
       audioElement.play();
-      audioState.animationRef.current = requestAnimationFrame(updateTime);
+      animationRef.current = requestAnimationFrame(updateTime);
       handleClick("Play", AudioList[currentSongIndex]?.title);
     } else {
-      setPlayerState((prevState) => ({ ...prevState, isPlaying: false }));
       audioElement.pause();
       cancelAnimationFrame(animationRef.current!);
       handleClick("Pause", AudioList[currentSongIndex]?.title);
@@ -91,18 +98,15 @@ const Player: React.FC<PlayerProps> = () => {
 
   // To Mute and Unmute the audio player
   const toggleMuteUnmute = () => {
-    const audioElement = audioState.audioPlayer.current;
+    const audioElement = audioPlayer.current;
     if (!audioElement) return;
 
     audioElement.muted = !audioElement.muted;
-    const action = playerState.isMuted ? "Unmute" : "Mute";
-    const songTitle = AudioList[playerState.currentSongIndex]?.title;
+    setIsMuted(audioElement.muted);
 
-    setPlayerState((prevState) => ({
-      ...prevState,
-      isMuted: audioElement.muted,
-    }));
-
+    // Log the userAction to DB
+    const action = isMuted ? "Unmute" : "Mute";
+    const songTitle = AudioList[currentSongIndex]?.title;
     handleClick(action, songTitle);
   };
 
@@ -115,87 +119,63 @@ const Player: React.FC<PlayerProps> = () => {
     return `${minutesReturned}:${secondsReturned}`;
   };
 
-  // To control the audio seek duration
+  // To control the audio duration
   const handleChangeRange = () => {
-    if (!audioState.audioPlayer.current || !audioState.progressBar.current)
-      return;
+    if (!audioPlayer.current || !progressBar.current) return;
 
-    audioState.audioPlayer.current.currentTime = Number(
-      audioState.progressBar.current.value
-    );
-    audioState.progressBar.current.style.setProperty(
+    audioPlayer.current.currentTime = Number(progressBar.current.value);
+    progressBar.current.style.setProperty(
       "--seek-before-width",
-      `${
-        (Number(audioState.progressBar.current.value) / playerState.duration) *
-        100
-      }%`
+      `${(Number(progressBar.current.value) / duration) * 100}%`
     );
-    setPlayerState((prevState) => ({
-      ...prevState,
-      currentTime: Number(audioState.progressBar.current.value),
-    }));
-    audioState.progressBar.current.max = String(playerState.duration);
+    setCurrentTime(Number(progressBar.current.value));
+    progressBar.current.max = String(duration);
 
     // Log "seek" and song title
-    const songTitle = AudioList[playerState.currentSongIndex]?.title;
+    const songTitle = AudioList[currentSongIndex]?.title;
     handleClick("Seek", songTitle);
   };
 
   // To update the current time of the audio
   const updateTime = () => {
-    if (!audioState.audioPlayer.current || !audioState.progressBar.current)
-      return;
+    if (!audioPlayer.current || !progressBar.current) return;
 
-    audioState.progressBar.current.value = String(
-      audioState.audioPlayer.current.currentTime
-    );
-    audioState.progressBar.current.style.setProperty(
+    progressBar.current.value = String(audioPlayer.current.currentTime);
+    progressBar.current.style.setProperty(
       "$seek-before-width",
-      `${
-        (Number(audioState.progressBar.current.value) / playerState.duration) *
-        100
-      }%`
+      `${(Number(progressBar.current.value) / duration) * 100}%`
     );
-    setPlayerState((prevState) => ({
-      ...prevState,
-      currentTime: Number(audioState.progressBar.current.value),
-    }));
-    audioState.animationRef.current = requestAnimationFrame(updateTime);
+    setCurrentTime(Number(progressBar.current.value));
+    animationRef.current = requestAnimationFrame(updateTime);
   };
 
   // Rewind or forward the audio by a specified number of seconds
   const adjustPlaybackTime = (seconds: number) => {
-    if (!audioState.progressBar.current) return;
+    if (!progressBar.current) return;
 
-    audioState.progressBar.current.value = String(
-      Number(audioState.progressBar.current.value) + seconds
+    progressBar.current.value = String(
+      Number(progressBar.current.value) + seconds
     );
 
     handleChangeRange();
     const action = seconds > 0 ? "Forward" : "Backward";
-    const songTitle = AudioList[playerState.currentSongIndex]?.title;
+    const songTitle = AudioList[currentSongIndex]?.title;
     handleClick(action, songTitle);
   };
 
   // Switch to previous song
   const playPreviousSong = () => {
-    let newIndex = playerState.currentSongIndex - 1;
+    let newIndex = currentSongIndex - 1;
     if (newIndex < 0) {
       newIndex = playlist.length - 1;
     }
-    setPlayerState((prevState) => ({
-      ...prevState,
-      currentSongIndex: newIndex,
-      isPlaying: true,
-    }));
-    if (audioState.audioPlayer.current) {
-      audioState.audioPlayer.current.src = playlist[newIndex];
-      audioState.audioPlayer.current.play();
-      audioState.audioPlayer.current.onloadedmetadata = () => {
-        setPlayerState((prevState) => ({
-          ...prevState,
-          duration: Math.floor(audioState.audioPlayer.current.duration),
-        }));
+    setCurrentSongIndex(newIndex);
+    setIsPlaying(true);
+    if (audioPlayer.current) {
+      audioPlayer.current.src = playlist[newIndex];
+      audioPlayer.current.play();
+      audioPlayer.current.onloadedmetadata = () => {
+        setDuration(Math.floor(audioPlayer.current.duration)); // to avoid NaN in duration
       };
     }
     handleClick("Prev", AudioList[newIndex]?.title);
@@ -203,43 +183,32 @@ const Player: React.FC<PlayerProps> = () => {
 
   // switch to next song
   const playNextSong = () => {
-    let nextSongIndex = playerState.currentSongIndex + 1;
+    let nextSongIndex = currentSongIndex + 1;
+    // If it's the last song, loop back to the first song
     if (nextSongIndex >= playlist.length) {
       nextSongIndex = 0;
     }
-    setPlayerState((prevState) => ({
-      ...prevState,
-      currentSongIndex: nextSongIndex,
-      isPlaying: true,
-    }));
-
-    if (audioState.audioPlayer.current) {
-      audioState.audioPlayer.current.src = playlist[nextSongIndex];
-      audioState.audioPlayer.current.play();
-      audioState.audioPlayer.current.onloadedmetadata = () => {
-        setPlayerState((prevState) => ({
-          ...prevState,
-          duration: Math.floor(audioState.audioPlayer.current.duration),
-        }));
+    setCurrentSongIndex(nextSongIndex);
+    setIsPlaying(true);
+    if (audioPlayer.current) {
+      audioPlayer.current.src = playlist[nextSongIndex];
+      audioPlayer.current.play();
+      audioPlayer.current.onloadedmetadata = () => {
+        setDuration(Math.floor(audioPlayer.current.duration)); // to avoid NaN in duration
       };
     }
     handleClick("Next", AudioList[nextSongIndex]?.title);
   };
 
-  // for song title animation and loading timeout
   useEffect(() => {
     const textInterval = setInterval(() => {
-      setPlayerState((prevState) => ({
-        ...prevState,
-        textMove: prevState.textMove >= 155 ? -155 : prevState.textMove + 1,
-      }));
+      setTextMove((prevTextMove) =>
+        prevTextMove >= 145 ? -145 : prevTextMove + 1
+      );
     }, 37);
 
     const loadingInterval = setTimeout(() => {
-      setAudioState((prevState) => ({
-        ...prevState,
-        loading: false,
-      }));
+      setLoading(false);
     }, 2000);
 
     return () => {
@@ -249,7 +218,7 @@ const Player: React.FC<PlayerProps> = () => {
   }, []);
 
   // Hard coded loading
-  if (audioState.loading) {
+  if (loading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <PuffLoader color="rgba(225, 253, 251, 1)" size={120} />
@@ -260,7 +229,7 @@ const Player: React.FC<PlayerProps> = () => {
   return (
     <div className="flex flex-col">
       {/* Animation */}
-      {playerState.isPlaying && (
+      {isPlaying && (
         <div
           data-aos="zoom-in-up"
           data-aos-duration="2000"
@@ -274,7 +243,7 @@ const Player: React.FC<PlayerProps> = () => {
         </div>
       )}
 
-      {playerState.isPlaying && (
+      {isPlaying && (
         <div
           data-aos="fade-up"
           data-aos-duration="1800"
@@ -285,36 +254,32 @@ const Player: React.FC<PlayerProps> = () => {
           {/* Duration line */}
           <div
             className="absolute left-1.5 bottom-0 h-1 bg-gradient-to-r from-red-500 via-pink-600 to-rose-900 transition-all duration-700"
-            style={{
-              width: `${
-                (playerState.currentTime / playerState.duration) * 100
-              }%`,
-            }}
+            style={{ width: `${(currentTime / duration) * 100}%` }}
           ></div>
 
           {/* Song Title and Poster */}
           <Image
             className="max-h-64 object-cover rounded-xl max-w-80"
-            src={AudioList[playerState.currentSongIndex]?.img?.src || ""}
-            alt={AudioList[playerState.currentSongIndex]?.title}
-            height={AudioList[playerState.currentSongIndex]?.img?.height || ""}
-            width={AudioList[playerState.currentSongIndex]?.img?.width || ""}
+            src={AudioList[currentSongIndex]?.img?.src || ""}
+            alt={AudioList[currentSongIndex]?.title}
+            height={AudioList[currentSongIndex]?.img?.height || ""}
+            width={AudioList[currentSongIndex]?.img?.width || ""}
           />
           <p
-            style={{ transform: `translateX(${playerState.textMove}%)` }}
+            style={{ transform: `translateX(${TextMove}%)` }}
             className="text-center py-1 mt-2 music-title"
           >
-            {AudioList[playerState.currentSongIndex]?.title}
+            {AudioList[currentSongIndex]?.title}
           </p>
         </div>
       )}
 
       {/* Audio Controls */}
       <div className="flex flex-col gap-1 justify-center items-center">
-        {playerState.isPlaying ? <ParticleStart /> : <ParticleEnd />}
+        {isPlaying ? <ParticleStart /> : <ParticleEnd />}
         <div
           className={`bg-black shadow-lg shadow-black/50 flex h-50 w-full rounded-lg p-3 items-center gap-1 ${
-            playerState.isPlaying
+            isPlaying
               ? "backdrop-blur-md bg-black/50 poster shadow-white/60 animate-down"
               : "animate-up"
           }`}
@@ -329,9 +294,9 @@ const Player: React.FC<PlayerProps> = () => {
             className="animate-down"
             onEnded={playNextSong}
             onLoadedMetadata={handleLoadedMetadata}
-            autoPlay={playerState.isPlaying ? true : false}
-            ref={audioState.audioPlayer}
-            src={AudioList[playerState.currentSongIndex]?.src}
+            autoPlay={isPlaying ? true : false}
+            ref={audioPlayer}
+            src={AudioList[currentSongIndex]?.src}
           ></audio>
           {/* Buttons */}
           {[
@@ -349,8 +314,8 @@ const Player: React.FC<PlayerProps> = () => {
             },
             {
               onClick: togglePlayPause,
-              src: playerState.isPlaying ? Images.Pause : Images.Play,
-              alt: playerState.isPlaying ? "pause" : "play",
+              src: isPlaying ? Images.Pause : Images.Play,
+              alt: isPlaying ? "pause" : "play",
               height: 28,
             },
             {
@@ -379,24 +344,22 @@ const Player: React.FC<PlayerProps> = () => {
 
           {/* Current time, duration, and progress bar */}
           <div className="text-white">
-            {calculateDuration(playerState.currentTime)}/
-            {playerState.duration &&
-              !isNaN(playerState.duration) &&
-              calculateDuration(playerState.duration)}
+            {calculateDuration(currentTime)}/
+            {duration && !isNaN(duration) && calculateDuration(duration)}
           </div>
           <input
             type="range"
             className="overflow-hidden progressBar hover:cursor-pointer"
             defaultValue="0"
-            ref={audioState.progressBar}
+            ref={progressBar}
             onChange={handleChangeRange}
           />
 
           {/* Toggle Mute/Unmute button */}
           <button onClick={toggleMuteUnmute}>
             <Image
-              src={playerState.isMuted ? Images.Mute : Images.Unmute}
-              alt={playerState.isMuted ? "unmute" : "mute"}
+              src={isMuted ? Images.Mute : Images.Unmute}
+              alt={isMuted ? "unmute" : "mute"}
               draggable="false"
               height={18}
             />
@@ -406,8 +369,7 @@ const Player: React.FC<PlayerProps> = () => {
         {/* View User Actions */}
         <a
           className={`text-white flex gap-2 m-3 shadow-md bg-gradient-to-br from-pink-500 to-red-800 hover:from-red-800 hover:to-pink-500 hover:bg-gradient-to-bl rounded-lg px-5 py-2.5 text-center transition-background duration-300 transform ${
-            playerState.isPlaying ? "animate-down" : "animate-up"
-
+            isPlaying ? "animate-down" : "animate-up"
           }`}
           href={"/pages/actions"}
         >
