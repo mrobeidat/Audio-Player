7ec84b127c840e1383dec4938672c7e420e17eec
mrobeidat <y.linux96@gmail.com>
2024-02-21 12:19:20 +0300

    Revert "clean code"
    This reverts commit 87ea42624a88ad5dad6503bbc6c3265f9411ce65.

--- src/app/pages/player/page.tsx
+++ src/app/pages/player/page.tsx
@@ -3,12 +3,13 @@ import React, { useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import { Images } from "../../components/Shared/Media/Images"; // Importing Images for audio controls
 import Audios from "../../components/Shared/Media/Audio"; // Importing the audio file
+import ParticleStart from "../../components/Shared/Particles/ParticleStart";
 import Footer from "../../layout/Footer";
 import AOS from "aos"; // animate on scroll
 import "aos/dist/aos.css"; // animate on scroll styles
+import ParticleEnd from "../../components/Shared/Particles/ParticleEnd";
 import { PuffLoader } from "react-spinners";
 import { AudioList } from "../../components/Shared/Media/AudioList";
-import {Particle} from "../../components/Shared/Particles/Particles";
 
 interface PlayerProps {}
 
@@ -276,7 +277,7 @@ const Player: React.FC<PlayerProps> = () => {
 
       {/* Audio Controls */}
       <div className="flex flex-col gap-1 justify-center items-center">
-        {<Particle isPlaying={isPlaying}  />}
+        {isPlaying ? <ParticleStart /> : <ParticleEnd />}
         <div
           className={`bg-black shadow-2xl shadow-black/50 flex h-50 w-full rounded-full p-3 items-center gap-1 ${
             isPlaying
