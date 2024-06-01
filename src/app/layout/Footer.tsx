74d1f4086cea4aa38e095ee89415c111412e1b50
mrobeidat <y.linux96@gmail.com>
2024-06-01 11:52:30 +0300

    responsive footer

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Images } from "../components/Shared/Media/Images";
 
 const Footer: React.FC = () => {
@@ -8,23 +8,23 @@ const Footer: React.FC = () => {
   return (
     <>
       <footer className="fixed bottom-0 left-0 right-0 backdrop-blur bg-black/30 text-center">
-        <div className="w-full max-w-screen-xl mx-auto p-2.5 md:py-2">
+        <div className="w-full max-w-screen-xl mx-auto p-2 md:p-4">
           <a
             href="https://yousef-portfolio.vercel.app"
             target="_blank"
             className="footer"
           >
-            <p className="font-sans">
+            <p className="text-sm md:text-base font-sans">
               {currentYear}&copy; Made with<span>❤️</span>by Yousef Obeidat
             </p>
           </a>
-          <div className="flex gap-3 justify-center mt-4">
+          <div className="flex gap-2 justify-center mt-2 md:mt-4">
             <a
               className="inline-block transform transition-transform ease-in-out duration-300 hover:scale-110"
               href="https://github.com/mrobeidat"
               target="_blank"
             >
-              <Image src={Images.Github} alt="github" width={30} height={30} />
+              <Image src={Images.Github} alt="github" width={24} height={24} />
             </a>
             <a
               className="inline-block transform transition-transform ease-in-out duration-300 hover:scale-110"
@@ -34,8 +34,8 @@ const Footer: React.FC = () => {
               <Image
                 src={Images.Linkedin}
                 alt="linkedin"
-                width={30}
-                height={30}
+                width={24}
+                height={24}
               />
             </a>
             <a
@@ -46,8 +46,8 @@ const Footer: React.FC = () => {
               <Image
                 src={Images.Portfolio}
                 alt="portfolio"
-                width={30}
-                height={30}
+                width={24}
+                height={24}
               />
             </a>
           </div>
